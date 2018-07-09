12b2751cf410c4dea9303c82b775b5a9cc8ed526
Jose Rafael Tongson <kanakanadelta@gmail.com>
2018-07-09 14:12:34 -0700

    initial commit with stringify and getElementsWithClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // start of code
+  var result = []; //result array for elements with 'className' class
+
+  var elementsWithClassName = function(element){
+
+    //check classList and if it contains the given className
+    if(element.classList && element.classList.contains(className)){
+      result.push(element);
+    }
+
+    //loop through childNodes if child nodes are present
+    if(element.childNodes){
+      for(var i = 0; i < element.childNodes.length; i++){
+        //recursion implementation
+        elementsWithClassName(element.childNodes[i]);
+      }
+    }
+  }
+
+  elementsWithClassName(document.body)
+  return result;
+  // end of function
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // start of code
+  if(typeof obj === 'string'){
+    return '\"' + obj.toString() +'\"';
+  }
+
+  if(obj === null) {
+    return 'null';
+  }
+  if(typeof obj ==='number' || typeof obj === 'boolean'){
+    return obj.toString();
+  }
+
+  // Arrays and Objects //
+  var output = '';
+
+  // Arrays: //
+
+  if(Array.isArray(obj)){
+    for(var i = 0; i < obj.length; i++){
+      output += stringifyJSON(obj[i]) + ',';
+    }
+    output = '[' + output.slice(0, output.length-1) + ']';
+  }
+
+  // Objects: //
+  if(!Array.isArray(obj) && typeof obj === 'object' && obj !== null){
+    var collection = {};
+    output += '{';
+
+    for(var key in obj){
+      if(typeof obj[key] !== 'function' && obj[key] !== undefined){
+        collection[key] = obj[key];
+      }
+    }
+
+    if(Object.keys(collection).length > 0){
+      for(var key in collection){
+        output += '\"' + key + '\":' + stringifyJSON(obj[key]) + ',';
+      }
+      output = output.slice(0, output.length-1);
+    }
+    output += '}';
+  }
+  return output + '';
+
+  // end of stringifyJSON function
 };
